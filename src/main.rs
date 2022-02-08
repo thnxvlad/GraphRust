c0f0f6684b11cc72c29844fae8bfed3c1bb7124d
Vladislav Eryomenko <80623364+thnxvlad@users.noreply.github.com>
2022-02-08 14:16:33 +0300

    display() fix

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use lib::Graph;
 fn main() {
     let file_path = String::from("input.tgf"); // put your file path
     let mut g: Graph<String, String> = Graph::read_from_file(file_path);
-    g.write_to_file(String::from("output.tgf"));
+    g.display();
 }
